9f7a4160f810d3d7c932b3582822ecd7e379d55b
MiKPo4eLiK <stassojcuk@gmail.com>
2025-05-24 14:28:54 +0300

    ghghgh

--- app/test_main.py
+++ app/test_main.py
@@ -20,18 +20,18 @@ def test_outdated_products_with_mocks() -> None:
 
     with mock.patch("app.main.datetime.date") as mock_date:
         mock_date.today.return_value = real_date(2022, 2, 2)
-        mock_date.side_effect = lambda *args, **kwargs: real_date(*args, **kwargs)
+        mock_date.side_effect = lambda *a, **kw: real_date(*a, **kw)
         result = outdated_products(products)
         assert result == ["duck"]
 
     with mock.patch("app.main.datetime.date") as mock_date:
         mock_date.today.return_value = real_date(2022, 2, 1)
-        mock_date.side_effect = lambda *args, **kwargs: real_date(*args, **kwargs)
+        mock_date.side_effect = lambda *a, **kw: real_date(*a, **kw)
         result = outdated_products(products)
         assert result == []
 
     with mock.patch("app.main.datetime.date") as mock_date:
         mock_date.today.return_value = real_date(2022, 2, 11)
-        mock_date.side_effect = lambda *args, **kwargs: real_date(*args, **kwargs)
+        mock_date.side_effect = lambda *a, **kw: real_date(*a, **kw)
         result = outdated_products(products)
         assert sorted(result) == sorted(["salmon", "chicken", "duck"])
